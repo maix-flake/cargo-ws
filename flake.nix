1c95df6990cfaf8b0fab4c6db656b942f0848892
Maix0 <maix522@gmail.com>
2022-11-24 13:06:04 +0100

    openssl

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    naersk,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+        cargo_workspace = pkgs.fetchFromGitHub {
+          owner = "pksunkara";
+          repo = "cargo-workspaces";
+          rev = "0da6389eccf4dee585607b72a0f11de9742cc1aa";
+          sha256 = "sha256-nOo6nnB7JI5wzOwoo1SRl6c4M9tvlFpr41ebtCm+YBo=";
+        };
+        naersk' = pkgs.callPackage naersk {};
+      in rec {
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage {
+          src = "${cargo_workspace}/cargo-workspaces";
+          nativeBuildInputs = with pkgs; [pkg-config];
+          fbuildInputs = with pkgs; [openssl];
+        };
+
+        # For `nix develop` (optional, can be skipped):
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [rustc cargo];
+        };
+      }
+    );
+}
