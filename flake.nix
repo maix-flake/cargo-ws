b826f740055d23a6ac29a27ba9c8a1761e19e7af
Maix0 <maix522@gmail.com>
2023-05-11 20:36:57 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    cargo-workspaces-git = {
+      url = "github:pksunkara/cargo-workspaces/v0.2.41"; # TODO: update ref to update upstream !
+      flake = false;
+    };
   };
 
   outputs = {
@@ -10,31 +14,23 @@
     flake-utils,
     naersk,
     nixpkgs,
+    cargo-workspaces-git,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = (import nixpkgs) {
           inherit system;
         };
-        cargo_workspace = pkgs.fetchFromGitHub {
-          owner = "pksunkara";
-          repo = "cargo-workspaces";
-          rev = "0da6389eccf4dee585607b72a0f11de9742cc1aa";
-          sha256 = "sha256-nOo6nnB7JI5wzOwoo1SRl6c4M9tvlFpr41ebtCm+YBo=";
-        };
         naersk' = pkgs.callPackage naersk {};
-      in rec {
-        # For `nix build` & `nix run`:
-        packages.default = naersk'.buildPackage {
-          src = "${cargo_workspace}/cargo-workspaces";
-          nativeBuildInputs = with pkgs; [pkg-config];
+        built = naersk'.buildPackage {
+          name = "cargo-workspaces";
+          src = "${cargo-workspaces-git}/cargo-workspaces";
+          nativeBuildInputs = with pkgs; [pkg-config perl];
           buildInputs = with pkgs; [openssl];
         };
-
-        # For `nix develop` (optional, can be skipped):
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [rustc cargo];
-        };
+      in {
+        packages.default = built;
+        apps = flake-utils.lib.mkApp {drv = built;};
       }
     );
 }
